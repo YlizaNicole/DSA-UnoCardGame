cc12bb5699638f429a3146f701f8dc497d64ce47
Yli <salazarylizanicole@gmail.com>
2023-02-15 23:39:21 +0800

    add card & printing

--- uno.py
+++ uno.py
@@ -1,11 +1,41 @@
-####### TRYING TO IMPROVE THE SRC CODE ########
-
+import random
 from enum import Enum
 
-
+#build a deck w colors
 class CardColor(Enum):
     blue = "#0956BF"
     red = "#D72600"
     yellow = "#ECD407"
     green = "#379711"
     black = "#222"
+
+class Card:
+    def __init__(self, number, colour):
+        self._number = number
+        self._colour = colour
+        self._amount = 0
+
+    def get_number(self):
+        return self._number
+
+    def set_number(self, number):
+        self._number = number
+
+    def get_colour(self):
+        return self._colour
+
+    def set_colour(self, colour):
+        self._colour = colour
+    
+    def get_pickup_amount(self):
+        return self._amount
+
+# def __init__(self, number, colour):
+print(Card(0, CardColor.red), (0, 10)),
+print(Card(0, CardColor.yellow), (0, 10)),
+print(Card(0, CardColor.green), (0, 10)),
+print(Card(0, CardColor.blue), (0, 10)),
+print(Card(0, CardColor.red), (1, 10)),
+print(Card(0, CardColor.yellow), (1, 10)),
+print(Card(0, CardColor.green), (1, 10)),
+print(Card(0, CardColor.blue), (1, 10)),
