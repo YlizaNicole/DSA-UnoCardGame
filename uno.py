f62a753f89362c422557c8fc3601c07e0325ff9d
Yli <salazarylizanicole@gmail.com>
2023-02-15 23:47:42 +0800

    adding turn manager

--- uno.py
+++ uno.py
@@ -45,22 +45,25 @@ class Card:
             return True
         return False
 
-    def play(self):
+    def play(self, player, game):
         return 0
+
 
 class SkipCard(Card):
     def __init__(self, number, colour):
         Card.__init__(self, number, colour)
         
-    def play(self, game):
+    def play(self, player, game):
         game.skip()
+
 
 class ReverseCard(Card):
     def __init__(self, number, colour):
         Card.__init__(self, number, colour)
 
-    def play(self, game):
+    def play(self, player, game):
         game.reverse()
+
 
 class Pickup2Card(Card):
     def __init__(self, number, colour):
@@ -87,50 +90,74 @@ class Pickup4Card(Card):
     def matches(self, putdown_pile):
         return True
 
-    def play(self, game):
+    def play(self, player, game):
         cards = game.pickup_pile.pick(self._amount)
         game.next_player().get_deck().add_cards(cards)
         game._turns._location = game._turns._location-1
+    
 
 class Deck:
     def __init__(self, starting_cards = None):
         if starting_cards == None:
-            self._cards = [] #DSA empty list
+            self._cards = []#DSA empty list
         else:
-            self._cards = [] # DSA empty list
-            self._cards.extend(starting_cards) #DSA List Methods & Built-In FunctIons extend()
+            self._cards = []#DSA empty list
+            self._cards.extend(starting_cards)#DSA List Methods & Built-In FunctIons extend()
  
     def get_cards(self):
         return self._cards
     
     def get_amount(self):
-        return len(self._cards) #DSA using List Methods & Built-In FunctIons len ()
+        return len(self._cards)#DSA using List Methods & Built-In FunctIons len ()
 
     def shuffle(self):
         random.shuffle(self._cards)
 
     def pick(self, amount = 1):
-        picked_cards = [] # DSA empty list
+        picked_cards = []#DSA empty list
         for i in range(0, amount):
-            picked_cards.append(self._cards[i]) #DSA List Methods & Built-In FunctIons append ()
+            picked_cards.append(self._cards[i])#DSA using List Methods & Built-In FunctIons len ()
+
         for i in range(0, amount):
             del self._cards[i]
         return picked_cards
 
     def add_card(self, card):
-        self._cards.append(card) #DSA List Methods & Built-In FunctIons
-
+        self._cards.append(card)
+#DSA using List Methods & Built-In FunctIons len ()
     def add_cards(self, cards):
-        self._cards.extend(cards) #DSA List Methods & Built-In FunctIons extend()
+        self._cards.extend(cards)#DSA List Methods & Built-In FunctIons extend()
 
     def top(self):
-        if len(self._cards) == 0: 
+        if len(self._cards) == 0:
             return None
         else:
-            #DSA using List Methods & Built-In FunctIons len ()
-            return self._cards[len(self._cards)-1]
+            return self._cards[len(self._cards)-1]#DSA using List Methods & Built-In FunctIons len ()
+
+
 
 class Player:
+    def __init__(self, name):
+        self._name = name
+        self._deck = Deck()
+
+    def get_name(self):
+        return self._name
+    
+    def get_deck(self):
+        return self._deck
+
+    def is_playable(self):
+        raise NotImplementedError("is_playable to be implemented by subclasses")
+    
+    def has_won(self):
+        if self._deck.get_amount() == 0:
+            return True
+        else:
+            return False
+
+    def pick_card(self, putdown_pile):
+        raise NotImplementedError("pick_card to be implemented by subclasses")
     def __init__(self, name):
         self._name = name
         self._deck = Deck()
