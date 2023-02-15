4e97087bfa8acf7d8661f1ef679db9fe9f0cf1a0
Yli <salazarylizanicole@gmail.com>
2023-02-15 23:37:50 +0800

    start

---
+++ uno.py
@@ -0,0 +1,11 @@
+####### TRYING TO IMPROVE THE SRC CODE ########
+
+from enum import Enum
+
+
+class CardColor(Enum):
+    blue = "#0956BF"
+    red = "#D72600"
+    yellow = "#ECD407"
+    green = "#379711"
+    black = "#222"
